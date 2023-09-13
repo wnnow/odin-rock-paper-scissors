81d4b28616e327e1d48066374a398ff28d1d3b65
wnnow <117201813+wnnow@users.noreply.github.com>
2023-09-14 04:59:54 +0700

    Add checkWinner() when score reach 5

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ function getComputerChoice() {
   let randomResult = rockPaperScissors[Math.floor(Math.random() * 3)];
   return randomResult;
 }
+
 let playerScore = 0;
 let computerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
   let playerSelectionLowerCase = playerSelection.toLowerCase();
   let computerSelectionLowerCase = computerSelection.toLowerCase();
@@ -64,13 +66,23 @@ const rpsSelectBtns = document.querySelectorAll("#rpsSelectBtn");
 
 const annouceMessage = document.querySelector(".announce-message");
 
+function checkWinner() {
+  if (computerScore === 5) {
+    annouceMessage.textContent = "Computer win this game!";
+  } else if (playerScore === 5) {
+    annouceMessage.textContent = "You WIN!";
+  } else return;
+}
+
 rpsSelectBtns.forEach((btn) =>
   btn.addEventListener("click", (e) => {
-    console.log(e.target.textContent);
+    if (playerScore === 5 || computerScore === 5) return;
+
     annouceMessage.textContent = `${playRound(
       e.target.textContent,
       getComputerChoice()
     )}`;
     updateScore();
+    checkWinner();
   })
 );
