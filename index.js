a8ad9506a65899bf57d9605081c0462d2082c6f6
wnnow <117201813+wnnow@users.noreply.github.com>
2025-11-24 10:32:36 +0700

    Implement playRound and rename getPlayerChoice
    Add playRound() to execute rounds sequentially and log outcomes.
    It prints each player's choice and the round result to the console,
    updates the winner's score, and increments the round counter.
    Rename getPlayerChoice() to getHumanChoice() to improve naming clarity.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let humanScore = 0;
+let computerScore = 0;
+let totalRound = 5;
+let round = 0;
 function getComputerChoice() {
   // random number 0-2 (total 3) each of number represent rock or paper or scissors
   let randomNum = Math.floor(Math.random() * 3);
@@ -18,9 +22,58 @@ function getComputerChoice() {
   return result;
 }
 
-function getPlayerChoice() {
-  const playerChoicePrompt = prompt("Rock, Paper or Scissors?", "Rock");
+function getHumanChoice() {
+  const humanChoicePrompt = prompt("Rock, Paper or Scissors?", "Rock");
   //lower case user prompt string incase of user prompt upper case string such as "ROCK" or "rOCk"
-  const playerChoiceLowerCase = playerChoicePrompt.toLowerCase();
-  return playerChoiceLowerCase;
+  const humanChoiceLowerCase = humanChoicePrompt.toLowerCase();
+  return humanChoiceLowerCase;
+}
+
+function playRound() {
+  const computerChoice = getComputerChoice();
+  const humanChoice = getHumanChoice();
+  console.log("You chose:", humanChoice);
+  console.log("Computer chose:", computerChoice);
+  if (humanChoice === "rock") {
+    if (computerChoice === "rock") {
+      console.log("Draw");
+    }
+    if (computerChoice === "paper") {
+      console.log("You lose this round!");
+      computerScore++;
+    }
+    if (computerChoice === "scissors") {
+      console.log("You win this round!");
+      humanScore++;
+    }
+  }
+  if (humanChoice === "paper") {
+    if (computerChoice === "paper") {
+      console.log("Draw");
+    }
+    if (computerChoice === "scissors") {
+      console.log("You lose this round!");
+      computerScore++;
+    }
+    if (computerChoice === "rock") {
+      console.log("You win this round!");
+      humanScore++;
+    }
+  }
+  if (humanChoice === "scissors") {
+    if (computerChoice === "scissors") {
+      console.log("Draw");
+    }
+    if (computerChoice === "rock") {
+      console.log("You lose this round!");
+      computerScore++;
+    }
+    if (computerChoice === "paper") {
+      console.log("You win this round!");
+      humanScore++;
+    }
+  }
+  console.log(`Round:${round}/${totalRound}`);
+  console.log(`Your score: ${humanScore}`);
+  console.log(`Computer score: ${computerScore}`);
 }
